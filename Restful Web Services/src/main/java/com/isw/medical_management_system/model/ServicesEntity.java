d8e024bcf07bb83a9fe3a58e8bbaa30838bb2577
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-11-30 19:22:44 +0200

    Making model class for services

---
+++ Restful Web Services/src/main/java/com/isw/medical_management_system/model/ServicesEntity.java
@@ -0,0 +1,25 @@
+package com.isw.medical_management_system.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "t_services")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ServicesEntity {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private int servicesID;
+    private String servicesName;
+    private float amount;
+
+}
