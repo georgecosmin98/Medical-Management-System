8476c11f7435cc1f3f84ca817a8d4dfb1f657a43
andreiz98 <61731341+andreiz98@users.noreply.github.com>
2020-12-02 17:06:14 +0200

    Make model class for patient

---
+++ Restful Web Services/src/main/java/com/isw/medical_management_system/service/PatientService.java
@@ -0,0 +1,28 @@
+package com.isw.medical_management_system.service;
+
+import com.isw.medical_management_system.model.PatientEntity;
+import com.isw.medical_management_system.model.ServicesEntity;
+import com.isw.medical_management_system.repository.PatientRepository;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+
+public class PatientService {
+
+    @Resource
+    private PatientRepository patientRepository;
+
+    public PatientEntity addPatient(PatientEntity patientEntity) {
+        patientRepository.save(patientEntity);
+        return patientEntity;
+    }
+
+    public List<PatientEntity> findAll() {
+        return patientRepository.findAll();
+    }
+}
