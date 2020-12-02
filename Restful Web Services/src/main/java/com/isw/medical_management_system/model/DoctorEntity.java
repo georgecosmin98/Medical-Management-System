0d545d04b778a6925a45c756ee444b993348be1a
andreiz98 <61731341+andreiz98@users.noreply.github.com>
2020-12-02 17:20:38 +0200

    Make model class for doctor

---
+++ Restful Web Services/src/main/java/com/isw/medical_management_system/model/DoctorEntity.java
@@ -0,0 +1,23 @@
+package com.isw.medical_management_system.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="t_patient")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class DoctorEntity extends PersonEntity{
+
+    private String department;
+    private String specialization;
+    
+}
