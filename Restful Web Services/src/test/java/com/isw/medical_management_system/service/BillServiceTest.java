f11a57aabc46994f1daadc2db918e9930c2d62ca
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-16 14:11:43 +0200

    Refactoring BillServiceTest class, eliminate duplicate code

--- Restful Web Services/src/test/java/com/isw/medical_management_system/service/BillServiceTest.java
+++ Restful Web Services/src/test/java/com/isw/medical_management_system/service/BillServiceTest.java
@@ -3,6 +3,7 @@ package com.isw.medical_management_system.service;
 import com.isw.medical_management_system.model.BillEntity;
 import com.isw.medical_management_system.model.ServicesEntity;
 import com.isw.medical_management_system.repository.BillRepository;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -15,6 +16,9 @@ import java.util.*;
 import static org.mockito.Mockito.*;
 
 public class BillServiceTest {
+    private BillEntity firstBill, secondBill;
+    private ServicesEntity servicesEntity;
+    private List bills,services;
 
     @Mock
     BillRepository billRepository;
@@ -25,28 +29,31 @@ public class BillServiceTest {
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
 
+    @Before
+    public void setUp() throws Exception {
+        bills = new LinkedList();
+        services = new ArrayList();
+
+        servicesEntity = new ServicesEntity("1", "Testing", 200);
+        services.add(servicesEntity);
+        firstBill = new BillEntity("1", services, new Date(2020, 12, 07));
+        secondBill = new BillEntity("2", services, new Date(2020, 10, 12));
+        bills.add(firstBill);
+        bills.add(secondBill);
+          }
+
     @Test
     public void shouldReturnCorrectCreateBill() {
-        ServicesEntity servicesEntity = new ServicesEntity("1", "Testing", 200);
-        List services = new ArrayList();
-        services.add(servicesEntity);
-        BillEntity billEntity = new BillEntity("1", services, new Date(2020, 12, 07));
 
         //Call method we want to test
-        billService.addBill(billEntity);
+        billService.addBill(firstBill);
 
         //Verify number of invocations method
-        verify(billRepository, times(1)).save(billEntity);
+        verify(billRepository, times(1)).save(firstBill);
     }
 
     @Test
     public void shouldReturnAllBills() {
-        List bills = new LinkedList();
-        ServicesEntity servicesEntity = new ServicesEntity("1", "Testing", 200);
-        List services = new ArrayList();
-        services.add(servicesEntity);
-        bills.add(new BillEntity("1", services, new Date(2020, 10, 12)));
-        bills.add(new BillEntity("2", services, new Date(2020, 10, 12)));
 
         //Return all mocked result set on find
         when(billRepository.findAll()).thenReturn(bills);
@@ -60,13 +67,9 @@ public class BillServiceTest {
 
     @Test
     public void shoulReturnCorrectBillById() {
-        ServicesEntity servicesEntity = new ServicesEntity("1", "Testing", 200);
-        List services = new ArrayList();
-        services.add(servicesEntity);
-        BillEntity billEntity = new BillEntity("1", services, new Date(2020, 10, 12));
 
         //Return all mocked result set on find
-        when(billRepository.findById("1")).thenReturn(Optional.of(billEntity));
+        when(billRepository.findById("1")).thenReturn(Optional.of(firstBill));
 
         //Call method we want to test
         billService.findById(1);
@@ -77,15 +80,11 @@ public class BillServiceTest {
 
     @Test
     public void shouldDeleteBillCorrectly() {
-        ServicesEntity servicesEntity = new ServicesEntity("1", "Testing", 50);
-        List services = new ArrayList();
-        services.add(servicesEntity);
-        BillEntity billEntity = new BillEntity("1", services, new Date(2020, 12, 02));
 
         //Call method we want to test
         billService.deleteById("1");
 
         //Verify number of invocations method
-        verify(billRepository, times(1)).deleteById(billEntity.getBillID());
+        verify(billRepository, times(1)).deleteById(firstBill.getBillID());
     }
 }
