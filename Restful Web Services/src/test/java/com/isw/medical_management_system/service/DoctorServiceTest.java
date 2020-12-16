e4a0b96634aca7b3e9317bc24815c3c6ad3a08b2
Ilinca George-Cosmin <ilincageorgecosmin@yahoo.com>
2020-12-16 14:20:25 +0200

    Refactoring DoctorServiceTest class, eliminate duplicate code

--- Restful Web Services/src/test/java/com/isw/medical_management_system/service/DoctorServiceTest.java
+++ Restful Web Services/src/test/java/com/isw/medical_management_system/service/DoctorServiceTest.java
@@ -2,6 +2,7 @@ package com.isw.medical_management_system.service;
 
 import com.isw.medical_management_system.model.DoctorEntity;
 import com.isw.medical_management_system.repository.DoctorRepository;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mockito.InjectMocks;
@@ -15,6 +16,7 @@ import java.util.List;
 import static org.mockito.Mockito.*;
 
 public class DoctorServiceTest {
+    private DoctorEntity firstDoctor, secondDoctor;
 
     @Mock
     DoctorRepository doctorRepository;
@@ -25,22 +27,26 @@ public class DoctorServiceTest {
     @Rule
     public MockitoRule rule = MockitoJUnit.rule();
 
-    @Test
-    public void shouldReturnCorrectCreateDoctor(){
-        DoctorEntity doctorEntity = new DoctorEntity("1","myName","myEmail","myNumber","myDep","mySpec",2000);
-
-        //Call method we want to test
-        doctorService.addDoctor(doctorEntity);
-
-        //Verify number of invocations method
-        verify(doctorRepository,times(1)).save(doctorEntity);
+    @Before
+    public void setUp() throws Exception {
+        firstDoctor = new DoctorEntity("1", "myName", "myEmail", "myNumber", "myDep", "mySpec", 2000);
+        secondDoctor = new DoctorEntity("2", "myName1", "myEmail1", "myNumber1", "myDep1", "mySpec1", 3000);
     }
 
     @Test
-    public void shouldReturnAllDoctors(){
+    public void shouldReturnCorrectCreateDoctor() {
+        //Call method we want to test
+        doctorService.addDoctor(firstDoctor);
+
+        //Verify number of invocations method
+        verify(doctorRepository, times(1)).save(firstDoctor);
+    }
+
+    @Test
+    public void shouldReturnAllDoctors() {
         List doctors = new LinkedList();
-        doctors.add(new DoctorEntity("1","myName","myEmail","myNumber","myDep","mySpec",2000));
-        doctors.add(new DoctorEntity("2","myName1","myEmail1","myNumber1","myDep1","mySpec1",3000));
+        doctors.add(firstDoctor);
+        doctors.add(secondDoctor);
 
         //Return all mocked result set on find
         when(doctorRepository.findAll()).thenReturn(doctors);
@@ -53,9 +59,7 @@ public class DoctorServiceTest {
     }
 
     @Test
-    public void shouldDeleteDoctor()
-    {
-        DoctorEntity doctorEntity = new DoctorEntity("1", "MyName", "MyEmailAdress", "myPhoneNumber", "ORL", "MyJob", 1500);
+    public void shouldDeleteDoctor() {
 
         //Call method we want to test
         doctorService.deleteById("1");
@@ -65,15 +69,15 @@ public class DoctorServiceTest {
     }
 
     @Test
-    public void shouldReturnUpdateDoctor(){
-        DoctorEntity doctorEntity1 = new DoctorEntity("1", "MyName", "MyEmailAdress", "myPhoneNumber", "ORL", "MyJob", 1500);
-        doctorService.addDoctor(doctorEntity1);
+    public void shouldReturnUpdateDoctor() {
 
-        doctorEntity1.setPhoneNumber("newPhone");
+        doctorService.addDoctor(firstDoctor);
+
+        firstDoctor.setPhoneNumber("newPhone");
         //Call method we want to test
-        doctorService.updateDoctor(doctorEntity1,"1");
+        doctorService.updateDoctor(firstDoctor, "1");
 
         //Verify if method was called
-        verify(doctorRepository).save(doctorEntity1);
+        verify(doctorRepository).save(firstDoctor);
     }
 }
